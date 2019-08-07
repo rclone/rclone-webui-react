29b6346c62df37c5aaedf4b6e1f5e1cfcfe3eee1
negative0 <bchaitanya15@gmail.com>
2019-08-07 22:35:49 +0530

    Removed unnecessary content from the header right aside.

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
-import {Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem} from 'reactstrap';
+import {Nav, NavItem} from 'reactstrap';
 import PropTypes from 'prop-types';
 
-import {AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler} from '@coreui/react';
+import {AppAsideToggler, AppNavbarBrand, AppSidebarToggler} from '@coreui/react';
 import logo from '../../assets/img/brand/logo.png'
 import favicon from '../../assets/img/brand/favicon.png'
 import BackendStatusCard from "../../views/Base/BackendStatusCard/BackendStatusCard";
@@ -37,32 +37,10 @@ class DefaultHeader extends Component {
                 </Nav>
                 <Nav className="ml-auto" navbar>
                     <BackendStatusCard mode={"button"}/>
-                    <NavItem className="d-md-down-none">
-                        <NavLink to="#" className="nav-link"><i className="icon-bell"></i>
-                            <Badge pill color="danger">5</Badge></NavLink>
-                    </NavItem>
-                    <NavItem className="d-md-down-none">
-                        <NavLink to="#" className="nav-link"><i className="icon-list"></i></NavLink>
-                    </NavItem>
-                    <NavItem className="d-md-down-none">
-                        <NavLink to="#" className="nav-link"><i className="icon-location-pin"></i></NavLink>
-                    </NavItem>
-                    <AppHeaderDropdown direction="down">
-                        <DropdownToggle nav>
-                            <img className="img-avatar d-none d-lg-block"
-                                 alt="admin@bootstrapmaster.com"/>
-                        </DropdownToggle>
-                        <DropdownMenu right style={{right: 'auto'}}>
-                            <DropdownItem header tag="div"
-                                          className="text-center"><strong>Account</strong></DropdownItem>
-                            <DropdownItem><i className="fa fa-bell-o"></i> Updates<Badge
-                                color="info">42</Badge></DropdownItem>
 
-                        </DropdownMenu>
-                    </AppHeaderDropdown>
+
                 </Nav>
                 <AppAsideToggler className="d-md-down-none"/>
-                {/*<AppAsideToggler className="d-lg-none" mobile />*/}
             </React.Fragment>
         );
     }
